5444e76516ba74de08d4859ceee0ab4511e0464b
aropai <ropai.anna@gmail.com>
2021-05-12 14:55:37 +0300

    Only replies to messages, to check server work

--- main.py
+++ main.py
@@ -14,25 +14,25 @@ updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-job = updater.job_queue
-job.run_daily(bot_functions.good_morning, time=datetime.time(hour=4, minute=00, second=00))
-job.run_daily(bot_functions.good_night, time=datetime.time(hour=18, minute=00, second=00))
-job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=7, minute=12, second=00))
-job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=10, minute=23, second=00))
-job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=14, minute=49, second=00))
-job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=12, minute=17, second=00))
+# job = updater.job_queue
+# job.run_daily(bot_functions.good_morning, time=datetime.time(hour=4, minute=00, second=00))
+# job.run_daily(bot_functions.good_night, time=datetime.time(hour=18, minute=00, second=00))
+# job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=7, minute=12, second=00))
+# job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=10, minute=23, second=00))
+# job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=14, minute=49, second=00))
+# job.run_daily(bot_functions.send_random_message, time=datetime.time(hour=12, minute=17, second=00))
 
 
-# @bot.message_handler(content_types=['text'])
-# def get_reply(message):
-#     if "хорошо" in message.text or "Хорошо" in message.text:
-#         bot.send_message(message.from_user.id, "Замечательно, очень рад за тебя)")
-#     elif "плохо" in message.text or "Плохо" in message.text:
-#         bot.send_message(message.from_user.id, "Зай, не грусти, ты со всем справишься, всё будет хорошо♥")
-#     else:
-#         bot.send_message(message.from_user.id, ")")
-#
-#
-# bot.polling(none_stop=True, interval=0)
+@bot.message_handler(content_types=['text'])
+def get_reply(message):
+    if "хорошо" in message.text or "Хорошо" in message.text:
+        bot.send_message(message.from_user.id, "Замечательно, очень рад за тебя)")
+    elif "плохо" in message.text or "Плохо" in message.text:
+        bot.send_message(message.from_user.id, "Зай, не грусти, ты со всем справишься, всё будет хорошо♥")
+    else:
+        bot.send_message(message.from_user.id, ")")
+
+
+bot.polling(none_stop=True, interval=0)
 updater.start_polling()
 updater.idle()
